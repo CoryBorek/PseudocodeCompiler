39bf06e41c06cfe03c6ae119e04845bc4cc1cff9
CoryBorek <agentdid127cb@aol.com>
2022-01-12 21:26:22 -0500

    Created Main class

---
+++ src/main/java/com/github/CoryBorek/PseudocodeCompiler/Main.java
@@ -0,0 +1,15 @@
+package com.github.CoryBorek.PseudocodeCompiler;
+
+/**
+ * Main class, runs program
+ */
+public class Main {
+
+    /**
+     * This function runs the entire program at root.
+     * @param args program arguments from command line.
+     */
+    public static void main(String[] args) {
+
+    }
+}
